d028564ba11fb871442928035166d046ff59c907
Lucas <lucas@MacBook-Air-de-Lucas.local>
2022-10-13 16:45:45 -0300

    feat: create doc to compareHash

--- src/@shared/utils/compareHash.ts
+++ src/@shared/utils/compareHash.ts
@@ -1,5 +1,9 @@
 import { compare } from "bcrypt";
 
+/*@function
+/*@param {string} key - This param is your plain text  key
+/*@param {string} hash - This param is yout hash to compare
+ */
 export function compareHash(key: string, hash: string) {
   return compare(key, hash);
 }
