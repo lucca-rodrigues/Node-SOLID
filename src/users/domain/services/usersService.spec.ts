9898fcbabb883fd1563a7185a238d490f87be9a1
Lucas <lucas@MacBook-Air-de-Lucas.local>
2022-11-03 18:37:09 -0300

    feat: add scenaries from find all users

--- src/users/domain/services/usersService.spec.ts
+++ src/users/domain/services/usersService.spec.ts
@@ -11,14 +11,30 @@ describe("should be testing the usersService", () => {
   });
 
   it("check if usersService return all users", async () => {
-    const generatedUser = [getDynamicUser()];
+    const generatedUser = getDynamicUser();
 
-    // jest.spyOn(usersService, "findAll").mockReturnValue(userStub);
     jest.spyOn(usersService, "findAll").mockReturnValue(generatedUser);
     const response = usersService.findAll();
 
-    console.log(response);
     expect(response).toEqual(generatedUser);
-    expect(response).toHaveLength(1);
+    expect(response["id"]).toBeTruthy();
+    expect(response["name"]).toBeTruthy();
+    expect(response["email"]).toBeTruthy();
+  });
+
+  it("check if usersService return multiple users", async () => {
+    const quantityUsers = 3;
+
+    const arrayUsers: UserEntity[] = [];
+
+    for (let index = 0; index < quantityUsers; index++) {
+      arrayUsers.push(getDynamicUser());
+    }
+
+    jest.spyOn(usersService, "findAll").mockReturnValue(arrayUsers);
+    const response = usersService.findAll();
+
+    expect(response).toEqual(arrayUsers);
+    expect(response).toHaveLength(quantityUsers);
   });
 });
